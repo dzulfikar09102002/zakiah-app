7280afa9a6db2874b941824f0e239f4eb76a6884
Dzulfikar Al Ghozali <dzulfikaralghozali9@gmail.com>
2026-02-10 23:54:38 +0700

    Update app-sidebar.tsx

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -21,12 +21,13 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard, employee } from '@/routes';
+import { dashboard } from '@/routes';
+import employees from '@/routes/employees';
+import roles from '@/routes/roles';
+import sellings from '@/routes/sellings';
 import type { NavItem } from '@/types';
 import AppLogo from './app-logo';
 import { NavSetting } from './nav-setting';
-import sellings from '@/routes/sellings';
-import roles from '@/routes/roles';
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
@@ -188,7 +189,7 @@ const mainNavItems: NavItem[] = [
             },
             {
                 title: "Karyawan",
-                href: employee().url,
+                href: employees.index().url,
             },
             {
                 title: "Role",
