3a7048c5eb04736904a3377dcffdf42e33c1a6ba
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2026-02-10 23:26:46 +0700

    route module management code

---
+++ app/Http/Controllers/SellingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class SellingController extends Controller
+{
+    public function summary()
+    {
+        return Inertia::render('report/sale/summary');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SellingController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -13,28 +14,25 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::prefix('administrasi/role') ->group(function (){
-    Route::get('/', [RoleController::class, 'index'])->middleware(['auth', 'verified'])->name('roles');
-    Route::post('/store', [RoleController::class, 'store'])->middleware(['auth', 'verified'])->name('roles.store');
-    Route::post('/{role}/update', [RoleController::class, 'update'])->middleware(['auth', 'verified'])->name('roles.update');
-    Route::delete('/{role}/delete', [RoleController::class, 'destroy'])->middleware(['auth', 'verified'])->name('roles.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+    Route::prefix('/roles')->group(function () {
+        Route::get('/', [RoleController::class, 'index'])->name('roles.index');
+        Route::post('/store', [RoleController::class, 'store'])->name('roles.store');
+        Route::post('/{role}/update', [RoleController::class, 'update'])->name('roles.update');
+        Route::delete('/{role}/delete', [RoleController::class, 'destroy'])->name('roles.destroy');
+    });
+    Route::prefix('/employees')->group(function () {
+        Route::get('/', [EmployeeController::class, 'index'])->name('employee');
+        Route::post('/store', [EmployeeController::class, 'index'])->name('employee.store');
+        Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create');
+    });
+    Route::prefix('sellings')->group(function () {
+        Route::get('/', [SellingController::class, 'summary'])->name('sellings.summary');
+    });
+    Route::prefix('/products')->group(function () {});
 });
 
-Route::prefix('administrasi/karyawan') ->group(function (){
-    Route::get('/', [EmployeeController::class, 'index'])->middleware(['auth', 'verified'])->name('employee');
-    Route::post('/store', [EmployeeController::class, 'index'])->middleware(['auth', 'verified'])->name('employee.store');
-    Route::get('/create', [EmployeeController::class, 'create'])->middleware(['auth', 'verified'])
-    ->name('employees.create');
-        
-});
-
-Route::prefix('laporan/penjualan') ->group(function (){
-    Route::get('/', function(){
-return Inertia::render('report/sale/summary');
-    })->middleware(['auth', 'verified'])->name('summary');
-});
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
