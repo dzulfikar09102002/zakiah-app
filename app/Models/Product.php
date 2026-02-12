2ea7bc210a13e9931acd346fb9eb16dcb9205cd3
Dzulfikar Al Ghozali <dzulfikaralghozali9@gmail.com>
2026-02-12 12:52:35 +0700

    fix-product-service

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function locationStocks()
+{
+    return $this->hasMany(ProductLocationStock::class, 'product_id', 'id');
+}
 }
