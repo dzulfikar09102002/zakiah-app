0ebb0e4b1917afeb9efcd71e8ed9d3cba6055206
Dzulfikar Al Ghozali <dzulfikaralghozali9@gmail.com>
2026-02-12 12:41:01 +0700

    service-product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
 
+    public function __construct(
+        protected ProductService $service
+    ) {}
     public function index()
     {
-        return Inertia::render('products/index');
+        $perPage = request('per_page', 10);
+
+        $entityId =  auth()->user()?->entity?->id; 
+
+        $products = $this->service->getProducts($entityId, $perPage);
+
+        $categories = $this->service->getCategories($entityId);
+
+        return Inertia::render('products/index', compact('products', 'categories'));
     }
 }
---
+++ app/Service/ProductService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use App\Models\ProductCategory;
+
+class ProductService
+{
+    public function getProducts(int $entityId, int $perPage = 10)
+    {
+        return Product::where('entity_id', $entityId)
+        ->withSum('locationStocks as total_stock', 'stock')
+        ->paginate($perPage)
+        ->withQueryString();
+    }
+
+    public function getCategories(int $entityId)
+    {
+        return ProductCategory::where('entity_id', $entityId);
+    }
+}
