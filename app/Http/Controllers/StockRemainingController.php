3b09bfa4c962f6a467b10103de34ba1a6ba9058d
Dzulfikar Al Ghozali <dzulfikaralghozali9@gmail.com>
2026-03-17 09:03:33 +0700

    Export Excell Stock Remaining

--- app/Http/Controllers/StockRemainingController.php
+++ app/Http/Controllers/StockRemainingController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Location;
 use App\Services\StockRemainingService;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
 class StockRemainingController extends Controller
 {
@@ -16,7 +23,10 @@ class StockRemainingController extends Controller
     {
         $locations = $this->service->getLocations();
 
-        return Inertia::render('reports/stocks/remainings/choose-location', compact('locations'));
+        return Inertia::render(
+            'reports/stocks/remainings/choose-location',
+            compact('locations')
+        );
     }
 
     public function report(Location $location)
@@ -25,6 +35,65 @@ class StockRemainingController extends Controller
         $categoryOptions = $this->service->getCategoryOptions();
         $locations = $this->service->getLocations();
 
-        return Inertia::render('reports/stocks/remainings/report', compact('pagination', 'categoryOptions', 'locations', 'location'));
+        return Inertia::render(
+            'reports/stocks/remainings/report',
+            compact('pagination', 'categoryOptions', 'locations', 'location')
+        );
+    }
+
+    public function export(Request $request, $locationId)
+    {
+        $location = Location::findOrFail($locationId);
+        $data = $this->service->getAllStockForExport($locationId);
+
+        $fileName = 'Stok-Sisa-' . str_replace(' ', '-', $location->name) . '.xlsx';
+
+        return Excel::download(
+            new StockRemainingExport($data),
+            $fileName
+        );
+    }
+}
+
+class StockRemainingExport implements FromCollection, WithHeadings, WithMapping, WithColumnFormatting
+{
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function collection()
+    {
+        return collect($this->data);
+    }
+
+    public function headings(): array
+    {
+        return ['SKU', 'Barcode', 'Nama', 'Kategori', 'Stok', 'HPP', 'Harga Jual'];
+    }
+
+    public function map($row): array
+    {
+        return [
+            (string) ($row['SKU'] ?? '-'),
+            (string) ($row['Barcode'] ?? '-'),
+            $row['Nama'] ?? '-',
+            $row['Kategori'] ?? '-',
+            $row['Stok'] ?? 0,
+            $row['HPP'] ?? 0,
+            $row['Harga Jual'] ?? 0,
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'A' => NumberFormat::FORMAT_TEXT,  
+            'B' => NumberFormat::FORMAT_TEXT,  
+            'F' => '_-"Rp "* #,##0_-',       
+            'G' => '_-"Rp "* #,##0_-',       
+        ];
     }
 }
